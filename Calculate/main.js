fc9431455ed7000849ef361947a3da39053ca099
Mohammad Reza esmailzadeh <smr8012101067@gmail.com>
2023-05-07 00:07:47 +0430

    fixed calculate project-2

--- Calculate/main.js
+++ Calculate/main.js
@@ -3,10 +3,27 @@ function showDisplay(event) {
   var x = event.target.innerText;
 
   if (displayBox.innerHTML == 0) {
+    if (x == "%" || x == "/" || x == "*" || x == "+" || x == "-") {
+      return;
+    }
     return (displayBox.innerHTML = x);
   }
 
-  return (displayBox.innerHTML += x);
+  let operators = [...document.querySelectorAll(".operator")].map(
+    (el) => el.textContent
+  );
+  let currentResult = displayBox.innerHTML;
+  console.log(currentResult.charAt(currentResult.length - 1));
+
+  if (
+    operators.includes(x) &&
+    operators.includes(currentResult.charAt(currentResult.length - 1))
+  ) {
+    displayBox.innerHTML =
+      currentResult.substring(0, currentResult.length - 1) + x;
+  } else {
+    return (displayBox.innerHTML += x);
+  }
 }
 
 function calculate() {
@@ -22,9 +39,8 @@ function clear() {
   let text = displayBox.innerText;
   if (text.length === 1) {
     displayBox.innerText = 0;
-  }
-  else {
-    displayBox.innerText = text.substring(0,text.length-1)
+  } else {
+    displayBox.innerText = text.substring(0, text.length - 1);
     // displayBox.innerText = ""
     // for (let i = 0 ; i <= text.length-2; i++) {
     //   displayBox.innerText += text[i]
